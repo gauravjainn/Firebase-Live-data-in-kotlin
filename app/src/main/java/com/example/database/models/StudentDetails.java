752d719f3ba6198008c0995ebc7771bf60a3892b
Gaurav Jain <gaurav.jain@nxgn.local>
2019-11-06 17:44:11 +0530

    insert delete added

---
+++ app/src/main/java/com/example/database/models/StudentDetails.java
@@ -0,0 +1,34 @@
+package com.example.database.models;
+
+
+public class StudentDetails {
+
+
+    private String name;
+    private String phoneNumber;
+
+    public StudentDetails() {
+        // This is default constructor.
+    }
+
+    public String getStudentName() {
+
+        return name;
+    }
+
+    public void setStudentName(String name) {
+
+        this.name = name;
+    }
+
+    public String getStudentPhoneNumber() {
+
+        return phoneNumber;
+    }
+
+    public void setStudentPhoneNumber(String phonenumber) {
+
+        this.phoneNumber = phonenumber;
+    }
+
+}
