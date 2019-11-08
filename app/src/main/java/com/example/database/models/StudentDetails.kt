878c09a6441104bc4e46c7f9abc9e736fcf93931
Rahul Panchal <rahul.panchal@thinkstart.in>
2019-11-08 19:22:15 +0530

    code committed

---
+++ app/src/main/java/com/example/database/models/StudentDetails.kt
@@ -0,0 +1,10 @@
+package com.example.database.models
+
+
+class StudentDetails {
+
+
+    var studentName: String? = null
+    var studentPhoneNumber: String? = null
+
+}// This is default constructor.
